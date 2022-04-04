cdc24870c6a145d9a68fbe6cfcf2629c361237d2
vasanth8888 <vasanthveeda09@gmail.com>
2022-04-04 20:14:45 +0530

    git13

---
+++ src/test/java/org/git13/Git13.java
@@ -0,0 +1,25 @@
+package org.git13;
+
+public class Git13 {
+	
+	private void jayasri() {
+		// TODO Auto-generated method stub
+   System.out.println(1);
+	}
+	
+	private void jaysri1() {
+		// TODO Auto-generated method stub
+  System.out.println(2);
+	}
+	
+	
+	
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
